8a12f0747135df1f1a28b34a9a75ab4bd966bb3b
vladislav <chasinvlad39@gmail.com>
2023-09-19 15:58:00 +0300

    header ready

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,12 @@
 import './style/index.scss'
-
+import menu from '../../source/images/header/Menu.svg'
 export default function Header(){
     return(
         <header>
             <div className="header_content">
-                <nav></nav>
+                <nav>
+                    <img src={menu} alt="" />
+                </nav>
                 <div className="header_description">
                     <div className="description_tag">
                         <p className="tag">Руководитель отдела</p>
