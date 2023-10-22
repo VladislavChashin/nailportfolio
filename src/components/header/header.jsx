a9d4416607b2526d9579c4d9d9fc7bfbfccb67f6
vladislav <chasinvlad39@gmail.com>
2023-10-22 14:15:09 +0300

    edit header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,10 +9,7 @@ export default function Header(){
                     <HamburgerMenu/>
                 </nav>
                 <div className="header_description wow animate__animated animate__fadeIn animate__slower">
-                    <div className="description_tag">
-                        <p className="tag">Руководитель отдела</p>
-                        <p className="tag">Руководитель проекта</p>
-                    </div>
+                    
                     <h1>Наиль Якубалиев</h1>
                 </div>
             </div>
